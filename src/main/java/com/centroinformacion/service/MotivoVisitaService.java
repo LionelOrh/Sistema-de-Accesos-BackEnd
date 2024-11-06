149d4ab37dfe39af24c5265ca319e65ca42da51d
Nando21MS <manriquef400@gmail.com>
2024-11-06 10:36:37 -0500

    feat Añadir visitante

---
+++ src/main/java/com/centroinformacion/service/MotivoVisitaService.java
@@ -0,0 +1,9 @@
+package com.centroinformacion.service;
+
+import com.centroinformacion.entity.MotivoVisita;
+
+public interface MotivoVisitaService {
+	
+	public void registrarMotivoVisita(MotivoVisita motivoVisita);
+	
+}
