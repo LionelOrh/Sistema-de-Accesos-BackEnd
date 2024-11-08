647dc56d22d8d9ff29e1a0d31739c857dc23ebc0
AstridYovera <ayoveratinoco@gmail.com>
2024-11-08 03:01:13 -0500

    feat: arreglos

--- src/main/java/com/centroinformacion/service/RegAccesosService.java
+++ src/main/java/com/centroinformacion/service/RegAccesosService.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import com.centroinformacion.entity.RegistroAcceso;
+import com.centroinformacion.entity.Representante;
 import com.centroinformacion.entity.TipoAcceso;
 
 public interface RegAccesosService {
@@ -11,4 +12,5 @@ public interface RegAccesosService {
 	 List<RegistroAcceso> listaPorTipoAcceso(TipoAcceso tipoAcceso);
 	 List<RegistroAcceso> listaPorFechaAcceso(LocalDate fechaAcceso);
 	 List<RegistroAcceso> listaConsultaCompleja(String login, LocalDate fechaAccesoDesde, LocalDate fechaAccesoHasta, int idTipoAcceso, String numDoc);   
+	 List<Representante> listaConsultaCompleta(String numDoc); 
 }   
